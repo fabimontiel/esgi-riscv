f44ba3d89cf2a32dcac8145e6eac0b3f2accd8f8
Fabiana <fabimontiel@gmail.com>
2020-03-25 00:12:29 +0100

    tp1 rendu

--- exercices/00_add.asm
+++ exercices/00_add.asm
@@ -3,12 +3,24 @@
 
 .text # <- segment de code.
 
+
+# Questions: C'est quoi un registre? (mettre un x dans la bonne réponse
+#
+# - [X] Une mémoire dans le processeur de 32 bits
+# - [ ] Une variable dans la mémoire
+
 # Exercice:
 # On veux obtenir le nombre 64 modifier le code pour pouvoir avoir 64
 # dans t0 à la fin du programme.
 
 mon_code: # <- Label
 li   t0, 32
-addi t0, t0, 0
+addi t0, t0, 32
 
 # Bonus: avoir 64 dans un registre en une seule instruction
+li t1, 64 # la plus simple et cool
+
+#li t2, 4096 # max = 4095
+
+addi t2, zero, 32
+add t3, t2, t2 # addition entre op1 (t2) op1 (t2); on stocke dans t2
--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -11,11 +11,25 @@ msg: .asciz "Hello ASM from .data"
 
 # Exercice: Observation dans Rars
 # - Quelle est l'addresse de base de la chaine msg? Son addresse de fin?
+#   -> addresse de base: 0x10010000
+#   -> addresse de fin:  0x10010014
 # - Dans quelle segment de la mémoire est elle?
+#   -> dans .data
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
+#   -> 0x6c6c6548  0x6f57206f  0x20646c72  0x6d6f7266  0x61642e20  0x00006174
+#   -> .data: 2e 64 61 74 61
 
-la a0, ??? # argument de l'appel systeme
-li a7, ??? # appel systeme 4 selon la doc de Rars printString
-ecall # Peret de basculer en mode kernel pour faire l'appel systeme.
+
+la a0, msg # argument de l'appel systeme
+li a7, 4 # appel systeme 4 selon la doc de Rars printString
+ # permet de basculer en mode kernel pour faire l'appel systeme
 
 # Bonus: Afficher maintenant un nombre, vous aurrez besoin de la documentation.
+# pour print un numéro 
+# li t0, 26
+# li  a7, 1          # 1 pour appeler à print int
+# add a0, t0, zero   # load desired value into argument register a0, using pseudo-op
+ecall
+
+
+
